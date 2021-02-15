8113265028cbbfe706aae1b16652a43fc137fff3
Vicente <vicentedpsantos@gmail.com>
2021-02-15 18:27:34 -0300

    reference and ownership
    ownership and references

--- exercise/e_ownership_references/src/main.rs
+++ exercise/e_ownership_references/src/main.rs
@@ -14,30 +14,55 @@ fn main() {
     // code with `cargo run apple` and `cargo run apples'.  Hint: use `.ends_with("s")` on the
     // String reference
     //
-    //inspect(&arg);
+    inspect(&arg);
 
     // 2. Write a function `change` that takes a *mutable* reference to a String and adds an "s" to
     // the String if it doesn't already end with "s". Then uncomment and run the code below with
     // `cargo run apple`.  Hint: use `.push_str("s")` on the mutable String reference to add an "s".
     //
-    //change(&mut arg);
-    //println!("I have many {}", arg);
+    change(&mut arg);
+    println!("I have many {}", arg);
 
     // 3. Write a function `eat` that accepts ownership of (consumes) a String and returns a bool
     // indicating whether or not the String both starts with a "b" AND contains an "a".
     // Hint 1: use `.starts_with("b")` and `.contains("a")`
     // Hint 2: `&&` is the boolean "AND" operator
-    //
-    //if eat(arg) {
-    //    println!("Might be bananas");
-    //} else {
-    //    println!("Not bananas");
-    //}
+
+    if eat(arg) {
+        println!("Might be bananas");
+    } else {
+        println!("Not bananas");
+    }
 
     // Try running this program with "boat", "banana", and "grapes" as the arguments :-)
 
     // Challenge: Write a function "add" that takes *references* to two integer arguments,
     // dereferences them and adds them together, and returns the result.
     //
-    // println!("1 + 2 = {}, even via references", add(&1, &2));
+    println!("1 + 2 = {}, even via references", add(&1, &2));
+}
+
+fn inspect(s: &String) {
+    if s.ends_with("s") {
+    	println!("plural");
+    } else {
+	println!("singular");
+    }
+}
+
+fn change(r: &mut String) {
+    if !r.ends_with("s") {
+    	r.push_str("s");
+    }
+}
+
+fn eat(food: String) -> bool {
+    let contains_a: bool = food.contains("a");
+    let starts_with_b: bool = food.starts_with("b");
+
+    contains_a && starts_with_b
+}
+
+fn add(x: &i32, y: &i32) -> i32 {
+    (*x) + (*y)
 }
